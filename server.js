0d2183dccfd39657f39b87146939a02b1672b75d
TheRainWolf <jesusm0562@gmail.com>
2026-05-01 22:12:35 -0400

    Agregar puerto MySQL a configuración de conexión

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 // Configuración de base de datos para Railway
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'vnrzzSYkAJtQqMIukCRGGonSELOkXYwf',
   database: process.env.DB_NAME || 'railway',
